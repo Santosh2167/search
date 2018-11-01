0d2cbbef97f4cc452ea2fed1c8a9aabdfc8c4ba4
Santosh <santosh.poudyal@gmail.com>
2018-11-01 22:33:40 +1100

    worked on advance search

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
 
     def self.search(search)
-        if search
+        if search && search!=""
             where(["name ILIKE ?","%#{search}%"]).or where(["category ILIKE ?","%#{search}%"])
+            #broken code >>> basic_search(category: "%#{search}%" ).basic_search(name: "%#{search}%")
         else
             all
         end
--- app/models/search.rb
+++ app/models/search.rb
@@ -3,7 +3,7 @@ class Search < ApplicationRecord
     def search_books
         books = Book.all
 
-        books = books.where(["name LIKE ?","%#{keywords}%"]) if keywords.present?
+        books = books.where(["name LIKE ?","%#{keyword}%"]) if keyword.present?
         books = books.where(["name LIKE ?","%#{category}%"]) if category.present?
         books = books.where(["name LIKE ?","%#{keywords}%"]) if min_price.present?
         books = books.where(["name LIKE ?","%#{keywords}%"]) if max_price.present?
